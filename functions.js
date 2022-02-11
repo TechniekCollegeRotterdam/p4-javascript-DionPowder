3815322eb95ad1cb1ace7faf4f3d113bbc29c99d
dion <dionvermeer@hotmail.com>
2022-02-11 12:01:29 +0100

    functions

---
+++ functions.js
@@ -0,0 +1,23 @@
+/*let x = myFunction(2, 8);   
+
+function myFunction(a, b) {
+  return a + b;             
+}
+
+console.log(x);*/
+
+/*const calculate = function(num1, num2){
+    let result = num1 + num2;
+    console.log(result);
+}
+
+calculate(10 + 2);*/
+
+const tellfortune = function(x, y, z, a, b) {
+    
+    
+    let result = "Je krijgt X kinderen, van Y verschillende partners genaam Z in het land A en je werkt als B";
+}
+
+
+tellfortune(2, 1, "Bart", "Nederland", "Accountant");
