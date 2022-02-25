b1d37ea840c455d1153582082df3a02deff74b12
dion <dionvermeer@hotmail.com>
2022-02-25 12:08:07 +0100

    js todo

--- functions.js
+++ functions.js
@@ -11,13 +11,52 @@ console.log(x);*/
     console.log(result);
 }
 
-calculate(10 + 2);*/
+calculate(10, 2);*/
 
-const tellfortune = function(x, y, z, a, b) {
+/*const tellfortune = function(x, y, z, a, b) {
     
     
     let result = "Je krijgt X kinderen, van Y verschillende partners genaam Z in het land A en je werkt als B";
 }
 
 
-tellfortune(2, 1, "Bart", "Nederland", "Accountant");
+tellfortune(2, 1, "Bart", "Nederland", "Accountant");*/
+
+
+/*const multiplyby5 = function(num){
+  let result = num * 5;
+  console.log(result);
+}
+
+multiplyby5(10);*/
+
+
+/*const checkInput = function(num, num4){
+  if (num > num4) {
+    console.log(num);}
+  
+  else  console.log(num4);
+}
+
+checkInput(30, 20);*/
+
+/*const calculate = function(num1, num2){
+  let result = num1 * num2;
+  console.log(result);
+}
+
+calculate(10, 2);
+
+const calculatedevide = function(num3, num4){
+  let result = num3 / num4;
+  console.log(result);
+}
+
+calculatedevide(10, 2);*/
+
+const calculateMinutes = function(sec){
+  let result = sec / 60;
+  console.log(result);
+}
+
+calculateMinutes(60)
